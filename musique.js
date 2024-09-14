0b9d66e1db213be6c5fb3f64517f62021ca6135e
tourdehanoi <lacanrafael@orange.fr>
2024-09-14 12:42:00 +0200

    button

--- musique.js
+++ musique.js
@@ -1,1 +1,27 @@
-const MusicList = document.getElementById("audioList")
+const MusicList = document.getElementById(".audioList")
+
+function changeState(x){
+    let btns = document.querySelectorAll(".btn");
+    let audio = document.querySelector("#myAudio")
+
+    for (let i=0; i<btns.length;i++){
+        btns[i].classList.remove("active");
+    }
+    btns[x].classList.add("active");
+
+    if(x==0){
+        audio.play();
+
+    }
+
+    if(x==1){
+        audio.pause();
+
+    }
+
+    if(x==2){
+        audio.pause();
+        audio.currentTime = 0;
+
+    }
+}
