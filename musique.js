ae4fa5f59b833b03a2971d0b31eb9e99ed78631b
tourdehanoi <lacanrafael@orange.fr>
2024-09-14 17:32:11 +0200

    j'abandonne

--- musique.js
+++ musique.js
@@ -25,3 +25,50 @@ function changeState(x){
 
     }
 }
+
+function getList(){
+    let list = [
+        "musique/AccumulaTown.mp3",
+        "musique/ForetKorogu.mp3",
+        "musique/gaur_plain.mp3",
+        "musique/GeantsMM.mp3",
+        "musique/HopesAndDreams.mp3",
+        "musique/LastDay.mp3",
+        "musique/littlerootTown.mp3",
+        "musique/LostWoods.mp3",
+        "musique/MaskedDedede.mp3",
+        "musique/RaviosTheme.mp3",
+        "musique/Reflection.mp3",
+        "musique/RosaSurf.mp3",
+        "musique/sm64staffroll.mp3",
+        "musique/SongOfHealing.mp3",
+        "musique/SongOfStorms.mp3",
+        "musique/TerminaField.mp3",
+        "musique/ThirdDistrict.mp3",
+        "musique/Zinnia.mp3"
+    ]
+    return list
+}
+
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+
+function PlayMusic(liste){
+    change = "musique/zinnia.mp3"
+    for(let i =0;i<liste.length;i++){
+        var audio = document.getElementById('myAudio');
+        var audio = audio.firstChild;
+        var src = audio.getAttribute("src").replace(change,liste[i]);
+        audio.setAttribute("src", src);
+        let current = document.querySelector("#myAudio");
+    }
+}
+
